d4f09b2e50f89e0a95f43abdee4ef8c2e4b5b69b
SmilingXinyi <smilingxinyi@gmail.com>
2022-01-12 15:26:18 +0800

    feat: 🎸 support glass material

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,9 @@ export default class Modelviewer {
         this.opts = Object.assign({}, {
             scale: .1,
             stats: false,
-            outputEncoding: 0
+            outputEncoding: 0,
+            opacity: .3,
+            rotationValue: .4
         }, opts);
 
         const widht = this.opts.size?.width || window.innerWidth;
@@ -114,16 +116,75 @@ export default class Modelviewer {
         if (modelType === 'gltf') {
             const loader = new GLTFLoader();
             loader.parse(modelsrc, '', gltf => {
+                console.log(gltf);
+
+                // @ts-ignore
+                if (gltf.scene.children[0]?.children[0]?.material?.opacity === 0) {
+                    // @ts-ignore
+                    gltf.scene.children[0]?.children[0]?.material?.opacity = this.opts.opacity
+                }
+
+                // @ts-ignore
+                if (gltf.scene.children[0]?.children[0]?.material?.name === '材质.3') {
+
+                    const material1 = new THREE.MeshPhysicalMaterial({
+                        roughness: 0.4,
+                        transmission: 2,
+                        // @ts-ignore
+                        thickness: 0.4,
+                        opacity: 0.5,
+                        transparent: true,
+                        sheen: 0.5,
+                        reflectivity: 0.7,
+                        clearcoat: 0.2
+                    });
+
+                    // @ts-ignore
+                    gltf.scene.children[0]?.children[0]?.material = material1;
+                }
+
+                // @ts-ignore
+                if (gltf.scene.children[0]?.children[0]?.material?.opacity === 0) {
+                    // @ts-ignore
+                    gltf.scene.children[0]?.children[0]?.material?.opacity = this.opts.opacity
+                    // @ts-ignore
+                    // gltf.scene.children[0]?.children[1]?.material?.roughness = 0.4;
+                    // @ts-ignore
+                    console.log(gltf.scene.children[0]?.children[0]?.material?.name);
+
+                    // @ts-ignore
+                    if (gltf.scene.children[0]?.children[0]?.material?.name === '材质.3') {
+                        console.log(true);
+                        // @ts-ignore
+                        gltf.scene.children[0]?.children[0]?.material?.roughness = 0.1;
+                        // @ts-ignore
+                        gltf.scene.children[0]?.children[0]?.material?.metaless = 0.5;
+
+                        // const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 128, {
+                        //     format: RGBFormat,
+                        //     generateMipmaps: true,
+                        //     minFilter: LinearMipmapLinearFilter
+                        // });
+
+                        // const cubeCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget)
+
+                        // const sphereMaterial = new THREE.MeshBasicMaterial({
+                        //     envMap: cubeCamera.renderTarget  //cubeCamera生成的纹理作为球体的环境贴图
+                        // })
+
+                    }
+                }
                 let mesh = gltf.scene.children[0];
                 mesh.position.set(0,0,0);
                 gltf.scene.scale.set(this.opts.scale, this.opts.scale, this.opts.scale);
                 this.scene.add(gltf.scene);
-                let dyal = 0.0025;
+                let dyal = 0.0015;
+                let rotationValue = this.opts.rotationValue;
                 this.animations.push(function () {
-                    if (gltf.scene.rotation.y >= .5) {
+                    if (gltf.scene.rotation.y >= rotationValue) {
                         dyal = -Math.abs(dyal);
                     }
-                    if (gltf.scene.rotation.y <= -.5) {
+                    if (gltf.scene.rotation.y <= -rotationValue) {
                         dyal = Math.abs(dyal);
                     }
                     gltf.scene.rotation.y = gltf.scene.rotation.y + dyal;
